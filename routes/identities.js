96201ca3c0266cb2c4e35c6dca1f5a72f87121b2
Robert Edstrom <legogris@legogris.se>
2014-02-19 14:42:49 +0100

    Refactoring identities route

--- routes/identities.js
+++ routes/identities.js
@@ -1,35 +1,56 @@
 module.exports = function(app, options) {
 
-	var nmc = options.nmc;
-	var settings = options.settings;
+    var _ = require('underscore'),
+	    nmc = options.nmc,
+        settings = options.settings,
+        endpoints = {};
+
+    var getEndpoints = function(id) {
+        return endpoints[id] || {};
+    };
+
+    var fetchPubKeys = function(id, onDone, onError) {
+		var keyNames = ['ssh-rsa'];
+		nmc.cmd('name_show', settings.nmcNamespace+'/'+id, function(err, data) {
+            if (err) {
+                onError(err);
+                return;
+            }
+            var val = JSON.parse(data.value),
+                result = {};
+            console.log(val);
+            for(var i=0; i<keyNames.length; i++) {
+                var key = keyNames[i];
+                if(val.hasOwnProperty(key)) {
+                    result[key] = val[key];
+                }
+            }
+            onDone(keys);
+        });
+    };
 
 	var findIdentity = function(req, res, next) {
 		res.setHeader('Access-Control-Allow-Origin','*');
-		var id = req.params[0];
-		var keys = ['ssh-rsa'];
+		var id = req.params[0],
+            result = {
+                id: id,
+                endpoints: getEndpoints(id);
+            };
 		console.log('querying "'+id+'"');
-		nmc.cmd('name_show', settings.nmcNamespace+'/'+id, function(err, data) {
-			if (err && err.code == -4) {
-				console.log('Id '+id+' not found in db.');
-				res.send(200, {id: id});
-				return;
-			}
-			if (err) {
-				console.log(err);
-				res.send(200, {});
-				return;
-			}
-			var val = JSON.parse(data.value);
-			console.log(val);
-			var result = {id: id};
-			for(var i=0; i<keys.length; i++) {
-				var key = keys[i];
-				if(val.hasOwnProperty(key)) {
-					result[key] = val[key];
-				}
-			}
-			res.send(200, result);
-		});
+        fetchPubKeys(id,
+            function(keys) {
+                result = _.extend(result, keys);
+            },
+            function(err) {
+                if(err.code === -4) {
+                    console.log('Id '+id+' not found in db.');
+                    res.send(404, {});
+                } else {
+                    console.log(err);
+                    res.send(500, {});
+                }
+            }
+        );
 		return next();
 	};
 
