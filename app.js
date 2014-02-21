29690612ba8e3204c482a551f918d6beadc9feb3
Robert Edstrom <legogris@legogris.se>
2014-02-21 14:32:11 +0100

    Change HTTP method from POST to PUT for endpints add, remove trailing add from URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var nmcRPC = require('nmcRPC');
 var settings = require('./settings.js');
 
 var server = restify.createServer({
-	name: 'nmc'
+    name: 'nmc'
 });
 
 var nmc = new nmcRPC.Client(settings.nmc);
@@ -14,11 +14,11 @@ server.use(restify.CORS());
 
 var identities = require('./routes/identities')(server, {'nmc': nmc, 'settings': settings});
 
-server.get({path: /^\/identities\/([0-9a-zA-Z-_.]+)$/, version: '0.0.1'}, identities.findIdentity);
-server.post({path: /^\/identities\/([0-9a-zA-Z-_.]+)\/endpoints\/add$/, version: '0.0.1'}, identities.addEndpoint);
+server.get({path: /^\/identities\/([0-9a-zA-Z-_.]+)$/, version: '0.0.2'}, identities.findIdentity);
+server.put({path: /^\/identities\/([0-9a-zA-Z-_.]+)\/endpoints\$/, version: '0.0.2'}, identities.addEndpoint);
 
 server.listen(settings.port, settings.ip, function(){
-	console.log('%s listening on %s', server.name, server.url);
+    console.log('%s listening on %s', server.name, server.url);
 });
 
 setInterval(identities.cleanupEndpoints, settings.endpointCleanupInterval);
